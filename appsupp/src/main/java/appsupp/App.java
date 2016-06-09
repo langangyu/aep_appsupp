f0ef3944d06b8bdc02efc9bf5f8ecf1e0de0adc9
langangyu <videon@gmail.com>
2016-06-08 23:18:07 -0600

    change object

--- appsupp/src/main/java/appsupp/App.java
+++ appsupp/src/main/java/appsupp/App.java
@@ -1,7 +1,7 @@
 package appsupp;
 
 public class App {
-    public static final Object APPLICATION_CODE = "EDS";
+    public static final String APPLICATION_CODE = "EDS";
 
 	public static String getHello() {
         return "Hello";
